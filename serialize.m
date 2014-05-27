59d65a79a1947754952e1f31de64fca10b7b3752
Markus Bergholz <markuman@gmail.com>
2014-05-27 23:48:39 +0200

    add license header from origin/master

--- serialize.m
+++ serialize.m
@@ -1,6 +1,18 @@
-%% Untested(!) Matlab Branch of github.com/octave-de/serialize
-%% Should work with GNU Octave as well
-%% GPLv3 - go https://github.com/octave-de/serialize for more!!
+%% Copyright (C) 2014 Andreas Weber <andy.weber.aw@gmail.com>
+%% Matlab branch Copyright (C) 2014 Markus Bergholz <markuman+gnupowered@gmail.com>
+%%
+%% This program is free software; you can redistribute it and/or modify it under
+%% the terms of the GNU General Public License as published by the Free Software
+%% Foundation; either version 3 of the License, or (at your option) any later
+%% version.
+%%
+%% This program is distributed in the hope that it will be useful, but WITHOUT
+%% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+%% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+%% details.
+%%
+%% You should have received a copy of the GNU General Public License along with
+%% this program; if not, see <http://www.gnu.org/licenses/>.
 
 function ret = serialize(obj)
   %% TODO:
